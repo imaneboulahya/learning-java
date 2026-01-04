d2be5bbbe940e19beab8e31e251137687c7e0a51
imaneboulahya <imaneboulahya705@gmail.com>
2026-01-04 22:01:57 +0100

    day 2 ex 1

---
+++ Day2/Day2/src/Ex1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Ex1 {
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrez votre nom : ");
+        String nom = sc.nextLine();
+        System.out.println("Bonjour "+nom+"!");
+    }
+}
