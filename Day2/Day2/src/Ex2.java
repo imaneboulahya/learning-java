87778f409a1a93e6523c85b78891697d78608129
imaneboulahya <imaneboulahya705@gmail.com>
2026-01-04 22:05:51 +0100

    day 2 ex 2

--- Day2/Day2/src/Ex2.java
+++ Day2/Day2/src/Ex2.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
 public class Ex2 {
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("entrez un nombre : ");
+        int nbr = sc.nextInt();
+        if (nbr % 2 == 0){
+            System.out.println("pair");
+        }
+        else {
+            System.out.println("impair");
+        }
+    }
 }
